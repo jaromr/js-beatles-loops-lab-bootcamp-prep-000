4ea4a088a3230969ab1e70a69c5e297e03b39707
Jarom Roney <byutrueblue@gmail.com>
2018-02-16 23:02:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
-  var myArray =
+  var myArray = []
+  for (i=0;i<musicians.length;i++){
+    myArray.push('${musicians[i] plays ${instruments[i]')
+  }
+  return myArray
 }
