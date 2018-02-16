106eaaaa47e89230b8ca46e95ffe20fb87a58768
Jarom Roney <byutrueblue@gmail.com>
2018-02-16 23:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
   var myArray = []
-  for (i=0;i<musicians.length;i++){
+  for (var i=0;i<musicians.length;i++){
     myArray.push('${musicians[i] plays ${instruments[i]')
   }
   return myArray
